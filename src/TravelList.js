a91131ae4f00cd4db132636902be66105f5ab640
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2022-09-25 13:28:41 +0100

    redirect updated

--- src/TravelList.js
+++ src/TravelList.js
@@ -2,19 +2,14 @@ import React from "react";
 import Card from "./Card";
 
 const TravelList = (props) => {
+  console.log(props)
   return (
     <ul className="card-list">
       {props.travels.map((travel) => {
         return (
           <Card
             key={travel.id}
-            id={travel.id}
-            imageUrl={travel.imageUrl}
-            title={travel.title}
-            startDate={travel.startDate}
-            endDate={travel.endDate}
-            description={travel.description}
-            googleMapsUrl={travel.googleMapsUrl}
+            {...travel}
           />
         );
       })}
